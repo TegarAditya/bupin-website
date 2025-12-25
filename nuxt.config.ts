adad29c14038a817c6e589c92cd498fdf76c2493
Tegar Aditya <tegaraditya53@gmail.com>
2025-12-25 10:53:37 +0700

    feat: setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   modules: [
-    '@nuxt/fonts',
-    '@nuxt/hints',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/fonts",
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+  ],
+});
